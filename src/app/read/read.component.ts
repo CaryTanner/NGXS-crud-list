837f654c36e3ca23e48f0f053dc20675197f3446
CaryTanner <cary.landon@gmail.com>
2021-02-24 07:37:02 +0100

    add output list

--- src/app/read/read.component.ts
+++ src/app/read/read.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Store, Select } from '@ngxs/store';
+import { Tutorial } from '../models/tutorial.model';
+import { TutorialState } from '../state/tutorial.state';
+import { Observable } from 'rxjs';
+import { RemoveTutorial } from '../actions/tutorial.actions';
 
 @Component({
   selector: 'app-read',
   templateUrl: './read.component.html',
-  styleUrls: ['./read.component.scss']
+  styleUrls: ['./read.component.scss'],
 })
 export class ReadComponent implements OnInit {
+  @Select(TutorialState.getTutorials) tutorials$!: Observable<Tutorial[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private store: Store) {
+    //this.tutorials$ = this.store.select((state) => state.tutorials.tutorials);
+    
   }
 
+  deleteTutorial(name: string) {
+    this.store.dispatch(new RemoveTutorial(name));
+  }
+
+  ngOnInit(): void {}
 }
